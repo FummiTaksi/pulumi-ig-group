36e1be4b3b371120c1578bfa36813017fbf75f5c
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2024-03-08 10:20:53 +0200

    create storage in gcp. Install @pulumi/gcp

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+
+new gcp.storage.Bucket("pulumi-ig-group-storage", {
+  name: "pulumi-ig-group-storage",
+  location: "EU",
+  forceDestroy: true,
+  uniformBucketLevelAccess: true,
+});
