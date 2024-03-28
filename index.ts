66ee96914933586f7c6bfbabad6ae9033119a73e
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2024-03-28 15:30:26 +0200

    add cloud function which still requires manually to allow unauthenticated traffic from cloud build

--- index.ts
+++ index.ts
@@ -2,11 +2,48 @@ import * as gcp from "@pulumi/gcp";
 import * as pulumi from "@pulumi/pulumi";
 
 const stack = pulumi.getStack();
-const bucketName = `pulumi-ig-group-storage-${stack}`;
+const bucketForMessages = `pulumi-ig-group-storage-${stack}`;
 
-new gcp.storage.Bucket(bucketName, {
-  name: bucketName,
+new gcp.storage.Bucket(bucketForMessages, {
+  name: bucketForMessages,
   location: "EU",
   forceDestroy: true,
   uniformBucketLevelAccess: true,
 });
+
+const bucketForCloudFunctions = new gcp.storage.Bucket(
+  `pulumi-ig-group-message-cloudfunction-storage-${stack}`,
+  {
+    name: `pulumi-ig-group-message-cloudfunction-storage-${stack}`,
+    location: "EU",
+    forceDestroy: true,
+    uniformBucketLevelAccess: true,
+  }
+);
+
+const createCloudFunction = true;
+
+if (createCloudFunction) {
+  const archive = new gcp.storage.BucketObject("archive", {
+    name: "message.zip",
+    bucket: bucketForCloudFunctions.name,
+    source: new pulumi.asset.FileAsset("./message-cloud-function/message.zip"),
+  });
+  new gcp.cloudfunctionsv2.Function(`message-${stack}`, {
+    name: `message-${stack}`,
+    buildConfig: {
+      entryPoint: "message",
+      runtime: "nodejs18",
+      source: {
+        storageSource: {
+          bucket: bucketForCloudFunctions.name,
+          object: archive.name,
+        },
+      },
+    },
+    serviceConfig: {
+      ingressSettings: "ALLOW_ALL",
+    },
+    location: "europe-west1",
+  });
+}
